18bc3f14bb2b3f4b1fbaaa5b289532fd8837151b
ashiqaa21 <ashikahaneef62@gmail.com>
2026-05-27 12:22:59 +0530

    new1

--- components/navbar.php
+++ components/navbar.php
@@ -8,7 +8,6 @@ $home_link = rtrim($base_path, '/') . '/index-temp';
 $about_link = rtrim($base_path, '/') . '/about/';
 $products_link = rtrim($base_path, '/') . '/products/';
 $contact_link = rtrim($base_path, '/') . '/contact/';
-$cart_link = rtrim($base_path, '/') . '/cart/';
 
 ?>
 <nav class="navbar fixed-top liyas-navbar">
@@ -32,7 +31,7 @@ $cart_link = rtrim($base_path, '/') . '/cart/';
     </ul>
 
     <!-- Cart Icon -->
-    <a href="<?php echo $cart_link; ?>" id="cart-icon" class="cart-icon-1" title="View Cart">
+    <a href="" id="cart-icon" class="cart-icon-1" title="View Cart">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c.51 0 .962-.343 1.087-.835l1.823-6.423a.75.75 0 00-.67-1.03H6.088l-.523-1.974M16.5 21a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM8.25 21a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
         </svg>
