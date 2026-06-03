522a9af335db27cbcf53405d63c6e5eee58eed40
thebrandweave <wearebrandweave@gmail.com>
2026-06-03 15:30:29 +0530

    added my orders

--- orders/order-details.php
+++ orders/order-details.php
@@ -45,6 +45,7 @@ $items = $itemStmt->fetchAll(PDO::FETCH_ASSOC);
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
      <link rel="stylesheet" href="../assets/css/cart.css">
+         <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/style.css">
 
 <style>
 body{
